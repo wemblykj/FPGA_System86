a6d455315f880fd0f2d3ab54690a9e091c388f44
Paul Wightmore <paul.wightmore@virginmedia.com>
2019-03-29 17:40:19 +0000

    Organising libraries into verilog and vhdl directories. Libraries are feature of VHDL and so I shall have to make VHDL wrappers/implementations for everything I wish to export from a library.

---
+++ src/lib/mc6809_cpu/hdl/vhdl/mc68a09e.vhd
@@ -0,0 +1,15 @@
+entity mc68a09e is
+	generic
+	(
+	)
+	port
+	(
+	
+	);
+
+end mc68a09e;
+
+architecture rtl of mc68a09e is
+
+begin
+end rtl;
---
+++ src/lib/system86/hdl/vhdl/system86.vhd
@@ -0,0 +1,15 @@
+entity system86 is
+	generic
+	(
+	)
+	port
+	(
+	
+	);
+
+end system86;
+
+architecture rtl of system86 is
+
+begin
+end rtl;
