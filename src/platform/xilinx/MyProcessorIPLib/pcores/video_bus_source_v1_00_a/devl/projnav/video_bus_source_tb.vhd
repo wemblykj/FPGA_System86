c6701d88eb4b4cbd02711f1b6a0ee63bc0805672
Paul Wightmore <paul.wightmore@virginmedia.com>
2020-01-05 19:49:22 +0000

    Abandoning attempt to introduce a configurable (directional) video bus breakout box. Instead I have instroduced a separate video bus source IP

---
+++ src/platform/xilinx/MyProcessorIPLib/pcores/video_bus_source_v1_00_a/devl/projnav/video_bus_source_tb.vhd
@@ -0,0 +1,148 @@
+-- TestBench Template 
+
+  LIBRARY ieee;
+  USE ieee.std_logic_1164.ALL;
+  USE ieee.numeric_std.ALL;
+
+  LIBRARY video_bus_breakout_v1_00_a;
+  USE video_bus_breakout_v1_00_a.video_bus_breakout;
+  
+  ENTITY testbench IS
+  END testbench;
+
+  ARCHITECTURE behavior OF testbench IS 
+
+          COMPONENT VGA_Sync_Pulses
+			 PORT(
+                  i_Clk : IN std_logic;
+						i_Rst : IN std_logic;
+						o_Locked : OUT std_logic;
+						o_HSync : OUT std_logic;
+						o_VSync : OUT std_logic
+                  );
+          END COMPONENT;
+			 
+			 COMPONENT Sync_To_Blanking
+			 PORT(
+                  i_Clk : IN std_logic;
+						i_Rst : IN std_logic;
+						i_HSync : IN std_logic;
+						i_VSync : IN std_logic;
+						o_Locked : OUT std_logic;
+						o_HSync : OUT std_logic;
+						o_VSync : OUT std_logic;
+						o_HBlank : OUT std_logic;
+						o_VBlank : OUT std_logic
+                  );
+			END COMPONENT;
+  -- Component Declaration
+--          COMPONENT <component name>
+--			 generic
+--          (
+--	       	  C_DIRECTION                    : integer              := 0;
+--              C_COMPONENT_DEPTH              : integer              := 8;
+--              C_USE_BLANKING                 : integer              := 0
+--          );
+--          PORT(
+--              I_HSYNC                        : in std_logic;
+--	           I_VSYNC                        : in std_logic;
+--	           I_HBLANK                       : in std_logic := 'X';
+--	           I_VBLANK                       : in std_logic := 'X';
+--	           I_RED                          : in std_logic_vector(C_COMPONENT_DEPTH-1 downto 0);
+--	           I_GREEN                        : in std_logic_vector(C_COMPONENT_DEPTH-1 downto 0);
+--	           I_BLUE                         : in std_logic_vector(C_COMPONENT_DEPTH-1 downto 0);
+--	 
+--	           O_HSYNC                        : out std_logic;
+--	           O_VSYNC                        : out std_logic;
+--	           O_HBLANK                       : out std_logic := 'X';
+--	           O_VBLANK                       : out std_logic := 'X';
+--	           O_RED                          : out std_logic_vector(C_COMPONENT_DEPTH-1 downto 0);
+--	           O_GREEN                        : out std_logic_vector(C_COMPONENT_DEPTH-1 downto 0);
+--	           O_BLUE                         : out std_logic_vector(C_COMPONENT_DEPTH-1 downto 0)
+--          );
+--          END COMPONENT;
+
+          SIGNAL VClk :  std_logic;
+			 SIGNAL Rst :  std_logic;
+			 
+			 SIGNAL Pattern : std_logic_vector(3 downto 0) := "0110";
+			 
+			 SIGNAL S1_Locked :  std_logic;
+			 SIGNAL S1_HSync :  std_logic;
+			 SIGNAL S1_VSync :  std_logic;
+			 
+			 SIGNAL UUT_Locked :  std_logic;
+			 SIGNAL UUT_HSync :  std_logic;
+			 SIGNAL UUT_VSync :  std_logic;
+			 SIGNAL UUT_HBlank :  std_logic;
+			 SIGNAL UUT_VBlank :  std_logic;
+          SIGNAL UUT_Red :  std_logic_vector(3 downto 0);
+			 SIGNAL UUT_Green :  std_logic_vector(3 downto 0);
+			 SIGNAL UUT_Blue :  std_logic_vector(3 downto 0);
+
+          CONSTANT VClk_Period : time := 1us;
+
+  BEGIN
+
+          stage1_timings: VGA_Sync_Pulses
+			 PORT MAP(
+                  i_Clk => VClk,
+                  i_Rst => Rst,
+						i_HSync => S1_HSync,
+						i_VSync => S1_VSync,
+						o_Locked => S1_Locked,
+						o_HSync => S1_HSync,
+						o_VSync => S1_VSync
+          );
+			 
+  -- Component Instantiation
+          uut: entity video_bus_breakout_v1_00_a.video_bus_breakout
+			 GENERIC MAP
+			 (
+			   C_BUS_FLAGS => 7,
+				C_COMPONENT_DEPTH => 4,
+				C_USE_BLANKING => 1
+			 )
+			 PORT MAP(
+                  -- I_CLK => VClk,
+                  -- I_RST => Rst,
+						I_HSYNC => S1_HSync,
+						I_VSYNC => S1_VSync,
+						I_HBLANK => S1_HBlank,
+						I_VBLANK => S1_VBlank,
+						-- O_LOCKED => UUT_Locked,
+						O_HSYNC => UUT_HSync,
+						O_VSYNC => UUT_VSync,
+						O_HBLANK => UUT_HBlank,
+						O_VBLANK => UUT_VBlank,
+						O_RED => UUT_Red,
+						O_GREEN => UUT_Green,
+						O_BLUE => UUT_Blue
+          );
+
+
+  --  Test Bench Statements
+     tb : PROCESS
+     BEGIN
+
+        wait for 100 ns; -- wait until global set/reset completes
+
+        -- Add user defined stimulus here
+
+        Rst <= '1';
+		  wait for 100 ns;
+		  Rst <= '0';
+		  
+        wait; -- will wait forever
+     END PROCESS tb;
+	  
+	  vid_clk : PROCESS
+     BEGIN
+        VClk <= '0';
+        wait for VClk_Period/2;
+        VClk <= '1';
+		  wait for VClk_Period/2;
+     END PROCESS vid_clk;
+  --  End Test Bench 
+
+  END;
---
+++ src/platform/xilinx/MyProcessorIPLib/pcores/video_bus_source_v1_00_a/hdl/vhdl/video_bus_source.vhd
@@ -0,0 +1,155 @@
+------------------------------------------------------------------------------
+-- video_bus_breakout.vhd - entity/architecture pair
+------------------------------------------------------------------------------
+-- IMPORTANT:
+-- DO NOT MODIFY THIS FILE EXCEPT IN THE DESIGNATED SECTIONS.
+--
+-- SEARCH FOR --USER TO DETERMINE WHERE CHANGES ARE ALLOWED.
+--
+-- TYPICALLY, THE ONLY ACCEPTABLE CHANGES INVOLVE ADDING NEW
+-- PORTS AND GENERICS THAT GET PASSED THROUGH TO THE INSTANTIATION
+-- OF THE USER_LOGIC ENTITY.
+------------------------------------------------------------------------------
+--
+-- ***************************************************************************
+-- ** Copyright (c) 1995-2012 Xilinx, Inc.  All rights reserved.            **
+-- **                                                                       **
+-- ** Xilinx, Inc.                                                          **
+-- ** XILINX IS PROVIDING THIS DESIGN, CODE, OR INFORMATION "AS IS"         **
+-- ** AS A COURTESY TO YOU, SOLELY FOR USE IN DEVELOPING PROGRAMS AND       **
+-- ** SOLUTIONS FOR XILINX DEVICES.  BY PROVIDING THIS DESIGN, CODE,        **
+-- ** OR INFORMATION AS ONE POSSIBLE IMPLEMENTATION OF THIS FEATURE,        **
+-- ** APPLICATION OR STANDARD, XILINX IS MAKING NO REPRESENTATION           **
+-- ** THAT THIS IMPLEMENTATION IS FREE FROM ANY CLAIMS OF INFRINGEMENT,     **
+-- ** AND YOU ARE RESPONSIBLE FOR OBTAINING ANY RIGHTS YOU MAY REQUIRE      **
+-- ** FOR YOUR IMPLEMENTATION.  XILINX EXPRESSLY DISCLAIMS ANY              **
+-- ** WARRANTY WHATSOEVER WITH RESPECT TO THE ADEQUACY OF THE               **
+-- ** IMPLEMENTATION, INCLUDING BUT NOT LIMITED TO ANY WARRANTIES OR        **
+-- ** REPRESENTATIONS THAT THIS IMPLEMENTATION IS FREE FROM CLAIMS OF       **
+-- ** INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS       **
+-- ** FOR A PARTICULAR PURPOSE.                                             **
+-- **                                                                       **
+-- ***************************************************************************
+--
+------------------------------------------------------------------------------
+-- Filename:          video_bus_breakout.vhd
+-- Version:           1.00.a
+-- Description:       Top level design, instantiates library components and user logic.
+-- Date:              Thu Nov 28 13:05:40 2019 (by Create and Import Peripheral Wizard)
+-- VHDL Standard:     VHDL'93
+------------------------------------------------------------------------------
+-- Naming Conventions:
+--   active low signals:                    "*_n"
+--   clock signals:                         "clk", "clk_div#", "clk_#x"
+--   reset signals:                         "rst", "rst_n"
+--   generics:                              "C_*"
+--   user defined types:                    "*_TYPE"
+--   state machine next state:              "*_ns"
+--   state machine current state:           "*_cs"
+--   combinatorial signals:                 "*_com"
+--   pipelined or register delay signals:   "*_d#"
+--   counter signals:                       "*cnt*"
+--   clock enable signals:                  "*_ce"
+--   internal version of output port:       "*_i"
+--   device pins:                           "*_pin"
+--   ports:                                 "- Names begin with Uppercase"
+--   processes:                             "*_PROCESS"
+--   component instantiations:              "I_<#|FUNC>"
+------------------------------------------------------------------------------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+LIBRARY video_lib_v1_00_a;
+USE video_lib_v1_00_a.video_bus_breakout;
+
+------------------------------------------------------------------------------
+-- Entity section
+------------------------------------------------------------------------------
+-- Definition of Generics:
+--   C_DIRECTION                  -- 
+--   C_COMPONENT_DEPTH            -- 
+--   C_USE_BLANKING               -- 
+--
+-- Definition of Ports:
+------------------------------------------------------------------------------
+
+ENTITY video_bus_breakout IS
+	GENERIC (
+		-- ADD USER GENERICS BELOW THIS LINE ---------------
+		C_COMPONENT_DEPTH : INTEGER := 8;
+		C_USE_CLOCK : INTEGER := 0;
+		C_USE_TIMINGS : INTEGER := 1;
+		C_USE_BLANKING : INTEGER := 0;
+		C_USE_DATA : INTEGER := 1
+		-- ADD USER GENERICS ABOVE THIS LINE ---------------
+
+	);
+	PORT (
+		-- ADD USER PORTS BELOW THIS LINE ------------------
+		I_CLK : IN std_logic;
+		I_HSYNC : IN std_logic;
+		I_VSYNC : IN std_logic;
+		I_HBLANK : IN std_logic := 'X';
+		I_VBLANK : IN std_logic := 'X';
+		I_LOCKED : IN std_logic;
+		I_RED : IN std_logic_vector(C_COMPONENT_DEPTH - 1 DOWNTO 0);
+		I_GREEN : IN std_logic_vector(C_COMPONENT_DEPTH - 1 DOWNTO 0);
+		I_BLUE : IN std_logic_vector(C_COMPONENT_DEPTH - 1 DOWNTO 0);
+
+		O_CLK : OUT std_logic;
+		O_HSYNC : OUT std_logic;
+		O_VSYNC : OUT std_logic;
+		O_HBLANK : OUT std_logic := 'X';
+		O_VBLANK : OUT std_logic := 'X';
+		O_LOCKED : OUT std_logic;
+		O_RED : OUT std_logic_vector(C_COMPONENT_DEPTH - 1 DOWNTO 0);
+		O_GREEN : OUT std_logic_vector(C_COMPONENT_DEPTH - 1 DOWNTO 0);
+		O_BLUE : OUT std_logic_vector(C_COMPONENT_DEPTH - 1 DOWNTO 0)
+		-- ADD USER PORTS ABOVE THIS LINE ------------------
+	);
+
+END ENTITY video_bus_breakout;
+
+------------------------------------------------------------------------------
+-- Architecture section
+------------------------------------------------------------------------------
+
+ARCHITECTURE IMP OF video_bus_breakout IS
+
+BEGIN
+
+	BUS_BREAKOUT_I : ENTITY video_lib_v1_00_a.video_bus_breakout
+		GENERIC MAP
+		(
+			C_COMPONENT_DEPTH => C_COMPONENT_DEPTH,
+			C_USE_CLOCK => C_USE_CLOCK,
+			C_USE_TIMINGS => C_USE_TIMINGS,
+			C_USE_BLANKING => C_USE_BLANKING,
+			C_USE_DATA => C_USE_DATA
+		)
+		PORT MAP
+		(
+			I_CLK => I_CLK,
+			I_HSYNC => I_HSYNC,
+			I_VSYNC => I_VSYNC,
+			I_HBLANK => I_HBLANK,
+			I_VBLANK => I_VBLANK,
+			I_LOCKED => I_LOCKED,
+			I_RED => I_RED,
+			I_GREEN => I_GREEN,
+			I_BLUE => I_BLUE,
+
+			O_HSYNC => O_HSYNC,
+			O_VSYNC => O_VSYNC,
+			O_HBLANK => O_HBLANK,
+			O_VBLANK => O_VBLANK,
+			O_LOCKED => O_LOCKED,
+			O_RED => O_RED,
+			O_GREEN => O_GREEN,
+			O_BLUE => O_BLUE
+		);
+
+END IMP;
