07f38ae788c1d6eaa06302c2b4ef742c4ab4c322
Paul Wightmore <paul.wightmore@virginmedia.com>
2021-02-18 00:44:36 +0000

    Having issues on master AXI bus when attaching AXI rom.

--- src/platform/xilinx/MyProcessorIPLib/pcores/axi_rom_v1_00_a/hdl/vhdl/axi_rom.vhd
+++ src/platform/xilinx/MyProcessorIPLib/pcores/axi_rom_v1_00_a/hdl/vhdl/axi_rom.vhd
@@ -111,6 +111,111 @@ library axi_rom_v1_00_a;
 -- S_AXI_ACLK            -- AXI Clock
 -- S_AXI_ARESETN          -- AXI Reset
 
+-- AXI Master Signals
+--
+--  M_AXI_ACLK               -- AXI Clock
+--  M_AXI_ARESETN            -- AXI Reset Signal - active low
+--
+-- AXI Write address channel signals
+--  M_AXI_AWID               -- Write address ID. This signal is the
+--                           -- identification tag for the write address 
+--                           -- group of signals
+--  M_AXI_AWLEN              -- Burst length. The burst length gives the 
+--                           -- exact number of transfers in a burst
+--  M_AXI_AWSIZE             -- Burst size. This signal indicates the size 
+--                           -- of each transfer in the burst
+--  M_AXI_AWBURST            -- Burst type. The burst type, coupled with
+--                           -- the size information, details how the address 
+--                           -- for each transfer within the burst is calculated
+--  M_AXI_AWADDR             -- Write address bus - The write address bus gives
+--                              the address of the first transfer in a write
+--                              burst transaction - fixed to 32
+--  M_AXI_AWCACHE            -- Cache type. This signal indicates 
+--                           -- the bufferable,cacheable, write-through, 
+--                           -- write-back,and allocate attributes of the
+--                           -- transaction 
+--  M_AXI_AWPROT             -- Protection type - This signal indicates the
+--                              normal, privileged, or secure protection level
+--                              of the transaction and whether the transaction
+--                              is a data access or an instruction access
+--  M_AXI_AWVALID            -- Write address valid - This signal indicates
+--                              that valid write address & control information
+--                              are available
+--  M_AXI_AWREADY            -- Write address ready - This signal indicates
+--                              that the slave is ready to accept an address
+--                              and associated control signals
+--  M_AXI_AWLOCK             -- Lock type. This signal provides additional 
+                             -- information about the atomic characteristics
+                             -- of the transfer
+--
+-- AXI Write data channel signals
+--
+--  M_AXI_WDATA              -- Write data bus  
+--  M_AXI_WSTRB              -- Write strobes - These signals indicates which
+--                              byte lanes to update in memory
+--  M_AXI_WLAST              -- Write last. This signal indicates the last 
+--                           -- transfer in a write burst
+--  M_AXI_WVALID             -- Write valid - This signal indicates that valid
+--                              write data and strobes are available
+--  M_AXI_WREADY             -- Write ready - This signal indicates that the
+--                              slave can accept the write data
+-- AXI Write response channel signals
+--
+--  M_AXI_BID                -- Response ID. The identification tag of the 
+--                           -- write response
+--  M_AXI_BRESP              -- Write response - This signal indicates the
+--                              status of the write transaction
+--  M_AXI_BVALID             -- Write response valid - This signal indicates
+--                              that a valid write response is available
+--  M_AXI_BREADY             -- Response ready - This signal indicates that
+--                              the master can accept the response information
+--
+-- AXI Read address channel signals
+--
+--  M_AXI_ARID               -- Read address ID. This signal is the 
+--                           -- identification tag for the read address group 
+--                           -- of signals
+--  M_AXI_ARADDR             -- Read address - The read address bus gives the
+--                              initial address of a read burst transaction
+--  M_AXI_ARPROT             -- Protection type - This signal provides
+--                              protection unit information for the transaction
+--  M_AXI_ARCACHE            -- Cache type. This signal provides additional 
+--                              information about the cacheable 
+--                           -- characteristics of the transfer
+--  M_AXI_ARVALID            -- Read address valid - This signal indicates,
+--                              when HIGH, that the read address and control
+--                              information is valid and will remain stable
+--                              until the address acknowledge signal,ARREADY,
+--                              is high.
+--  M_AXI_ARLEN              -- Burst length. The burst length gives the 
+--                           -- exact number of transfers in a burst
+--  M_AXI_ARSIZE             -- Burst size. This signal indicates the size of i
+                             -- each transfer in the burst
+--  M_AXI_ARBURST            -- Burst type. The burst type, coupled with the 
+--                           -- size information, details how the address for
+                             -- each transfer within the burst is calculated
+--  M_AXI_ARLOCK             -- Lock type. This signal provides additional 
+                             -- information about the atomic characteristics
+                             -- of the transfer
+--  M_AXI_ARREADY            -- Read address ready - This signal indicates
+--                              that the slave is ready to accept an address
+--                              and associated control signals:
+--
+-- AXI Read data channel signals
+--
+--  M_AXI_RID                -- Read ID tag. This signal is the ID tag of 
+                             -- the read data group of signals
+--  M_AXI_RDATA              -- Read data bus - fixed to 32
+--  M_AXI_RRESP              -- Read response - This signal indicates the
+--                              status of the read transfer
+--  M_AXI_RVALID             -- Read valid - This signal indicates that the
+--                              required read data is available and the read
+--                              transfer can complete
+--  M_AXI_RLAST              -- Read last. This signal indicates the 
+--                           -- last transfer in a read burst
+--  M_AXI_RREADY             -- Read ready - This signal indicates that the
+--                              master can accept the read data and response
+--                              information
 -- AXI Slave Lite signals
 -- S_AXI_ACLK            -- AXI Clock
 -- S_AXI_ARESETN          -- AXI Reset
@@ -151,10 +256,12 @@ entity axi_rom is
       C_INSTANCE                    : string  := "axi_rom_inst";
       
       -- Master AXI Generics
-      -- C_M_AXI_THREAD_ID_WIDTH       : integer := 4;
+      --C_M_AXI_THREAD_ID_WIDTH       : integer := 4;
       C_M_AXI_ADDR_WIDTH            : integer := 32;
       C_M_AXI_DATA_WIDTH            : integer := 32;
-      
+      C_M_AXI_PROTOCOL              : string                    := "AXI4";
+		C_M_AXI_THREAD_ID_WIDTH       : integer                   := 4;
+		
 		-- Slave AXI-Lite Generics
 		C_S_AXI_ADDR_WIDTH            : integer range 8 to 8 := 8;
       C_S_AXI_DATA_WIDTH            : integer range 32 to 32 := 32
@@ -175,31 +282,63 @@ entity axi_rom is
   
       -- AXI Global
 
-      -- AXI Master Write Address Channel
+ -- AXI signals
       M_AXI_ACLK              : in  std_logic; -- AXI clock
       M_AXI_ARESETN           : in  std_logic; -- AXI active low synchronous reset
-     
-      -- AXI Master Read Address Channel
-      --M_AXI_ARID              : out std_logic_vector(C_M_AXI_THREAD_ID_WIDTH-1 downto 0);
-      M_AXI_ARADDR            : out std_logic_vector(C_M_AXI_ADDR_WIDTH-1 downto 0);
-      M_AXI_ARLEN             : out std_logic_vector(7 downto 0);
-      M_AXI_ARSIZE            : out std_logic_vector(2 downto 0);
-      M_AXI_ARBURST           : out std_logic_vector(1 downto 0);
-      M_AXI_ARPROT            : out std_logic_vector(2 downto 0);
-      M_AXI_ARVALID           : out std_logic;
-      M_AXI_ARREADY           : in  std_logic;
-      M_AXI_ARLOCK            : out std_logic;
-      M_AXI_ARCACHE           : out std_logic_vector(3 downto 0);
 
-      -- AXI Master Read Data Channel
-      M_AXI_RDATA             : in  std_logic_vector(C_M_AXI_DATA_WIDTH-1 downto 0);
-      M_AXI_RRESP             : in  std_logic_vector(1 downto 0);
-      M_AXI_RLAST             : in  std_logic;
-      M_AXI_RVALID            : in  std_logic;
-      M_AXI_RREADY            : out std_logic;
-
-      -- AXI Slave Lite Interface - CFG Block
-		S_AXI_ACLK              : in  std_logic;
+ -- AXI Write Address Channel Signals
+    --M_AXI_AWID         : out std_logic_vector 
+    --                         (C_M_AXI_THREAD_ID_WIDTH-1 downto 0);
+    M_AXI_AWLEN        : out std_logic_vector (7 downto 0);
+    M_AXI_AWSIZE       : out std_logic_vector (2 downto 0);
+    M_AXI_AWBURST      : out std_logic_vector (1 downto 0);
+    --M_AXI_AWCACHE      : out std_logic_vector (3 downto 0);
+    M_AXI_AWADDR       : out std_logic_vector(C_M_AXI_ADDR_WIDTH-1 downto 0);
+    --M_AXI_AWPROT       : out std_logic_vector(2 downto 0);
+    M_AXI_AWVALID      : out std_logic;
+    M_AXI_AWREADY      : in  std_logic;
+    M_AXI_AWLOCK       : out std_logic;
+ -- AXI Write Data Channel Signals
+	 M_AXI_WID         : out std_logic_vector 
+                             (C_M_AXI_THREAD_ID_WIDTH-1 downto 0);
+    M_AXI_WDATA        : out std_logic_vector(C_M_AXI_DATA_WIDTH-1 downto 0);
+    M_AXI_WSTRB        : out std_logic_vector
+                             ((C_M_AXI_DATA_WIDTH/8)-1 downto 0);
+    M_AXI_WLAST        : out std_logic;
+    M_AXI_WVALID       : out std_logic;
+    M_AXI_WREADY       : in  std_logic;
+    
+ -- AXI Write Response Channel Signals
+    --M_AXI_BID          : in  std_logic_vector 
+    --                         (C_M_AXI_THREAD_ID_WIDTH-1 downto 0);
+    M_AXI_BRESP        : in  std_logic_vector(1 downto 0);
+    M_AXI_BVALID       : in  std_logic;
+    M_AXI_BREADY       : out std_logic;
+	 
+ -- AXI Read Address Channel Signals
+    --M_AXI_ARID         : out std_logic_vector 
+    --                         (C_M_AXI_THREAD_ID_WIDTH-1 downto 0);
+    M_AXI_ARLEN        : out std_logic_vector(7 downto 0);
+    M_AXI_ARSIZE       : out std_logic_vector(2 downto 0);
+    M_AXI_ARBURST      : out std_logic_vector(1 downto 0);
+    --M_AXI_ARPROT       : out std_logic_vector(2 downto 0);
+    --M_AXI_ARCACHE      : out std_logic_vector(3 downto 0);
+    M_AXI_ARVALID      : out std_logic;
+    M_AXI_ARADDR       : out std_logic_vector(C_M_AXI_ADDR_WIDTH-1 downto 0);
+    M_AXI_ARLOCK       : out std_logic;
+    M_AXI_ARREADY      : in  std_logic;
+	 
+ -- AXI Read Data Channel Sigals
+    --M_AXI_RID          : in  std_logic_vector 
+    --                         (C_M_AXI_THREAD_ID_WIDTH-1 downto 0);
+    M_AXI_RDATA        : in  std_logic_vector(C_M_AXI_DATA_WIDTH-1 downto 0);
+    M_AXI_RRESP        : in  std_logic_vector(1 downto 0);
+    M_AXI_RVALID       : in  std_logic;
+    M_AXI_RLAST        : in  std_logic;
+    M_AXI_RREADY       : out std_logic;
+ 
+   -- AXI Slave Lite Interface - CFG Block
+ 	 S_AXI_ACLK              : in  std_logic;
     S_AXI_ARESETN           : in  std_logic;
     S_AXI_AWADDR            : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 
     								downto 0);
