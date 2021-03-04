5a8fd130bc6645cb14fac22490ccd31439a97cb1
Paul Wightmore <paul.wightmore@virginmedia.com>
2021-03-04 19:15:57 +0000

    Added self test module

---
+++ src/platform/xilinx/MyProcessorIPLib/drivers/ttlmembus_v1_00_a/src/xttlmembus_selftest.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+*
+* (c) Copyright 2021 Paul Wightmore. All rights reserved.
+*
+* Driver stub based on GPIO driver
+*
+*****************************************************************************/
+int XTtlMemBus_SelfTest(XTtlMemBus * InstancePtr)
+{
+	Xil_AssertNonvoid(InstancePtr != NULL);
+	Xil_AssertNonvoid(InstancePtr->IsReady == XIL_COMPONENT_IS_READY);
+
+	/*
+	 * Read from the data register of channel 1 which is always guaranteed
+	 * to be in the hardware device. Since the data may be configured as
+	 * all inputs, there is not way to guarantee the value read so don't
+	 * test it.
+	 */
+	(void) XTtlMemBus_DiscreteRead(InstancePtr, 1);
+
+	return (XST_SUCCESS);
+}
