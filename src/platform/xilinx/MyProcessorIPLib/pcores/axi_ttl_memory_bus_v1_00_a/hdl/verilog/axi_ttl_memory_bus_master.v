9d44bd5ffb8f0af9962b4fa5443dd88882bb74d9
Paul Wightmore <paul.wightmore@virginmedia.com>
2021-03-22 17:53:06 +0000

    Started ttl mem bus read logic

--- src/platform/xilinx/MyProcessorIPLib/pcores/axi_ttl_memory_bus_v1_00_a/hdl/verilog/axi_ttl_memory_bus_master.v
+++ src/platform/xilinx/MyProcessorIPLib/pcores/axi_ttl_memory_bus_v1_00_a/hdl/verilog/axi_ttl_memory_bus_master.v
@@ -3,9 +3,9 @@ module axi_ttl_memory_bus_master #(
         parameter C_DATA_WIDTH = 8,
 		  parameter C_MST_AWIDTH = 32,
         parameter C_MST_DWIDTH = 32)
-       (input wire 				ChipEnable,
-        input wire 				OutputEnable,
-        input wire 				WriteEnable,
+       (input wire 				nChipEnable,
+        input wire 				nOutputEnable,
+        input wire 				nWriteEnable,
         input wire [C_ADDR_WIDTH-1:0] 	 	Address,
         inout wire [C_DATA_WIDTH-1:0] 	 	Data,
         input wire [C_MST_AWIDTH-1:0] 	MappedAddress,
@@ -26,8 +26,75 @@ module axi_ttl_memory_bus_master #(
     input wire bus2ip_mstwr_dst_rdy_n
 		  );
 
-always @(posedge ChipEnable) begin
+localparam
+	Idle = 3'b000,
+	AddressReq = 3'b001,
+	AddressMap = 3'b010,
+	OperationReq = 3'b011,
+	ReadReq = 3'b100,
+	WriteReq = 3'b101;
+
+reg[2:0] state_reg, state_next;
+
+reg [C_ADDR_WIDTH-1:0] busAddress;
+reg [C_MST_AWIDTH-1:0] mappedAddress;
+
+always @(state_next, posedge ip2bus_mst_reset) begin
+	if (ip2bus_mst_reset) begin
+		state_reg <= Idle;
+	end else begin
+		state_reg <= state_next;
+	end
 end
 
+always @(state_reg, nChipEnable, nOutputEnable, nWriteEnable) begin
+	state_next = state_reg;
+	
+	case(state_reg)
+		Idle:
+			 if (!nChipEnable)
+				if (busAddress !== Address)
+					state_next = AddressReq;
+					
+		AddressReq:
+			busAddress = Address;
+			// TODO: wait on interrupts
+			if (!nChipEnable)
+				state_next = AddressMap;
+			else 
+				state_next = Idle;
+				
+		AddressMap:
+			if (!nChipEnable) begin
+				mappedAddress <= MappedAddress + busAddress;
+				state_next = OperationReq;
+			end else
+				state_next = Idle;
+				
+		OperationReq:
+			if (!nChipEnable) begin
+				if (busAddress !== Address)
+					state_next = AddressReq;
+				if (nWriteEnable) begin
+					state_next = WriteReq;
+				end else if (nOutputEnable) begin
+					state_next = ReadReq;
+				end
+			end else
+				state_next = Idle;
+					
+		ReadReq:
+			if (!nChipEnable) begin
+				// TODO: wait on interrupts
+			end else
+				state_next = Idle;
+		WriteReq:
+			if (!nChipEnable) begin
+				// TODO: wait on interrupts
+			end else
+				state_next = Idle;
+	endcase
+end
+	
 endmodule
 
