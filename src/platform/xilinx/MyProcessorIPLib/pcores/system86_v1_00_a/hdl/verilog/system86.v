ea67c19e51f6c978e68dc7d949cce37325c88029
Paul Wightmore <paul.wightmore@virginmedia.com>
2021-03-29 17:38:01 +0100

    Added missing verilog source

---
+++ src/platform/xilinx/MyProcessorIPLib/pcores/system86_v1_00_a/hdl/verilog/system86.v
@@ -0,0 +1,6 @@
+// This module will take incoming horizontal and veritcal blank signals and
+// create Row and Column counters based on the active pixel area.
+// It will align the Row/Col counters to the output blank signals.
+// Useful for any module that needs to keep track of which Row/Col position we
+// are on in the active portion of a frame
+`include "../../../../../../../lib/system86/rtl/system86.v"
