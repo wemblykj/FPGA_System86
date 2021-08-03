d50f21983385a704f549671eaca06edf29ae9966
Paul Wightmore <paul.wightmore@virginmedia.com>
2021-08-04 00:40:50 +0100

    Tweaking rom/ram interconnects. Tweaking and improving some IP definitions. Added missing netlist for system86 IP

--- src/platform/xilinx/MyProcessorIPLib/pcores/axi_ttl_memory_bus_v1_00_a/data/axi_ttl_memory_bus_v2_1_0.tcl
+++ src/platform/xilinx/MyProcessorIPLib/pcores/axi_ttl_memory_bus_v1_00_a/data/axi_ttl_memory_bus_v2_1_0.tcl
@@ -22,8 +22,8 @@
 #
 proc iplevel_update_supports_write { param_handle } {
   set mhsinst [xget_hw_parent_handle $param_handle]
-  set readonly [xget_hw_parameter_value $mhsinst "C_READONLY"]
-  if { $readonly == 1 } {
+  set memtype [xget_hw_parameter_value $mhsinst "C_MEM_TYPE"]
+  if { $memtype == 1 } {
     return 0
   } else {
     return 1
