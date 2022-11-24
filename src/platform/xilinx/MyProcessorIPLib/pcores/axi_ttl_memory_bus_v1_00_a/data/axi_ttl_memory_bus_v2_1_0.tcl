0eaf73e06b3797f4f843afc651aaf8bafa9412c7
Paul Wightmore <paul.wightmore@virginmedia.com>
2022-11-24 17:08:43 +0000

    Updated mem test harness with membus encorder Added test ram to test harness

--- src/platform/xilinx/MyProcessorIPLib/pcores/axi_ttl_memory_bus_v1_00_a/data/axi_ttl_memory_bus_v2_1_0.tcl
+++ src/platform/xilinx/MyProcessorIPLib/pcores/axi_ttl_memory_bus_v1_00_a/data/axi_ttl_memory_bus_v2_1_0.tcl
@@ -75,10 +75,10 @@ proc iplevel_update_control_bus_size { param_handle } {
   set memtype [xget_hw_parameter_value $mhsinst "C_MEM_TYPE"]
   if { $memtype == 1 } {
     set romtype [xget_hw_parameter_value $mhsinst "C_ROM_TYPE"]
-    if { $romtype == 81 } {
+    if { $romtype == 1 } {
 	return 2
     } else {
-		return 1
+	return 1
     }
   } else {
 	#set bw [expr 3]
@@ -91,14 +91,16 @@ proc iplevel_update_control_bus_size { param_handle } {
 proc calc_mapped_size { mhsinst } {
   set addr_width [xget_hw_parameter_value $mhsinst "C_ADDR_WIDTH"]
   set data_width [xget_hw_parameter_value $mhsinst "C_DATA_WIDTH"]
-  set size [expr (int(pow(2.0, $addr_width) * $data_width) / 8)]
+  set size [expr (int(pow(2.0, $addr_width) * pow(2.0, $data_width)) / 8)]
   
   return $size
 }
 
 proc iplevel_update_mapped_size { param_handle } {
   set mhsinst [xget_hw_parent_handle $param_handle]
-  return [calc_mapped_size $mhsinst]
+  set size [calc_mapped_size $mhsinst]
+
+  return [format 0x%08X $size]
 }
 
 proc iplevel_update_mapped_high { param_handle } {
@@ -106,7 +108,11 @@ proc iplevel_update_mapped_high { param_handle } {
   set size [calc_mapped_size $mhsinst]
   
   set addr_base [xget_hw_parameter_value $mhsinst "C_MAPPED_BASEADDR"]
-  set addr_high [expr ($addr_base + $size) - 1]
+  if { $addr_base != 0xffffffff } {
+  	set addr_high [expr ($addr_base + $size) - 1]
+  } else {
+  	set addr_high 0xffffffff
+  }
   
   return [format 0x%08X $addr_high]
 }
